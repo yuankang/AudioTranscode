8bd311f51f67d6c6462ba1f5f8e998c599305438
yuankang <troyyuan@gmail.com>
2023-08-08 16:38:20 +0800

    modify compile.sh

--- compile.sh
+++ compile.sh
@@ -5,9 +5,10 @@ cd audio; rm -rf 000.pcm 111.pcm 222.aac 333.pcm; cd -;
 gcc AudioTranscode.c g711.c -lsoxr -lfdk-aac -I . -o AudioTranscode
 ./AudioTranscode
 
-ffplay -f alaw -ar 8000 -ac 1 audio/000.g711a
-#ffplay -f mulaw -ar 8000 -ac 1 audio/000.g711u
-ffplay -f s16le -ar 8000 -ac 1 audio/000.pcm
-ffplay -f s16le -ar 11025 -ac 1 audio/111.pcm
-ffplay audio/222.aac
-ffplay -f s16le -ar 11025 -ac 1 audio/333.pcm
+echo "---------- play cmd ----------" 
+echo "ffplay -f alaw -ar 8000 -ac 1 audio/000.g711a"
+echo "ffplay -f mulaw -ar 8000 -ac 1 audio/000.g711u"
+echo "ffplay -f s16le -ar 8000 -ac 1 audio/000.pcm"
+echo "ffplay -f s16le -ar 11025 -ac 1 audio/111.pcm"
+echo "ffplay audio/222.aac"
+echo "ffplay -f s16le -ar 11025 -ac 1 audio/333.pcm"
